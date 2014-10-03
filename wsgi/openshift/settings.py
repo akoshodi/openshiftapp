f33eae5a89910663fbb9705359f2dfa60c11a1ac
Akinwale Oshodi <akoshodi@gmail.com>
2014-10-03 05:24:32 +0100

    Added Wagtail CMS

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -1,4 +1,4 @@
-"""
+    """
 Django settings for openshift project.
 
 For more information on this file, see
@@ -53,6 +53,20 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'compressor',
+    'taggit',
+    'modelcluster',
+    
+    'wagtail.wagtailcore',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailredirects',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -62,6 +76,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
 )
 
 # If you want configure the REDISCLOUD
@@ -125,3 +141,48 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+
+)
+
+
+from django.conf import global_settings
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
+)
+
+
+EMAIL_SUBJECT_PREFIX = '[wagtailtutorial] '
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+# Auth settings
+LOGIN_URL = 'django.contrib.auth.views.login'
+LOGIN_REDIRECT_URL = 'wagtailadmin_home'
+
+
+# WAGTAIL SETTINGS
+WAGTAIL_SITE_NAME = 'maestro'
+
+# Override the search results template for wagtailsearch
+WAGTAILSEARCH_RESULTS_TEMPLATE = 'maestroblog/search_results.html'
+WAGTAILSEARCH_RESULTS_TEMPLATE_AJAX = 'maestroblog/includes/search_listing.html'
+
+WAGTAILSEARCH_ES_INDEX = 'maestro'
+
+AUTH_PROFILE_MODULE = 'userprofile.UserProfile'
+
+
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,10 +1,31 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 
+from site_settings.views import Index
+
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
 from django.conf import settings
+import os.path
+
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtailimages import urls as wagtailimages_urls
+from wagtail.wagtailembeds import urls as wagtailembeds_urls
+from wagtail.wagtaildocs import admin_urls as wagtaildocs_admin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailsnippets import urls as wagtailsnippets_urls
+from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls, admin as wagtailsearch_admin_urls
+from wagtail.wagtailusers import urls as wagtailusers_urls
+from wagtail.wagtailredirects import urls as wagtailredirects_urls
+
+# Signal handlers
+from wagtail.wagtailsearch import register_signal_handlers as wagtailsearch_register_signal_handlers
+wagtailsearch_register_signal_handlers()
 
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,5 +33,20 @@ urlpatterns = patterns('',
     # url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^blog-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^search/', include(wagtailsearch_frontend_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+
+    url(r'', include(wagtail_urls)),
+
 )
+
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
+    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
