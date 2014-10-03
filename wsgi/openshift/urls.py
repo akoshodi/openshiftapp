6732f11881085f0ee214df0aa5441dcaebc73266
Akinwale Oshodi <akoshodi@gmail.com>
2014-10-03 06:08:03 +0100

    settings2

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from site_settings.views import Index
+
 
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
