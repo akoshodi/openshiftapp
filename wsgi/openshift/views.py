4e4252487f9ec74855cc78d0a9aac3849c6596d1
Akinwale Oshodi <akoshodi@gmail.com>
2014-11-20 09:44:05 +0100

    Changed homepage background image

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render_to_response
 def home(request):
      return render_to_response('blog/index.html')
 
+
 def blog(request):
      return render_to_response('blog/blog.html')
