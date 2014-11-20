666250b3bfaf0806ed758795606b52514f3f4bd4
Akinwale Oshodi <akoshodi@gmail.com>
2014-11-20 05:17:06 +0100

    Minor change

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render_to_response
 def home(request):
      return render_to_response('blog/index.html')
 
-def home1(request):
+def blog(request):
      return render_to_response('blog/blog.html')
